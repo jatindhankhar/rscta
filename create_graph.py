c2b5aa554bd55cfa25e9941971f660ec7c67cda6
Jatin Dhankhar <dhankhar.jatin@gmail.com>
2016-03-09 22:10:14 +0530

    Wrapped the functionality inside a thin wrapper named draw

--- create_graph.py
+++ create_graph.py
@@ -4,11 +4,12 @@ from nltk.tokenize import RegexpTokenizer
 
 tokenizer = RegexpTokenizer(r'\w+') # Tokenizer with regex to remove punctuation marks
 
-input_file = codecs.open("file.txt") # Replace with the file
-input_data = input_file.read()
+def draw(file_name): # Usage expects file name
+ input_file = codecs.open(file_name) # Replace with the file
+ input_data = input_file.read()
 
-tokens = tokenizer.tokenize(input_data)
+ tokens = tokenizer.tokenize(input_data)
 
-fd = nltk.FreqDist(tokens)
+ fd = nltk.FreqDist(tokens)
 
-fd.plot(30,cumulative=False) # Plots a non cumulative graph for 30 common words
+ fd.plot(30,cumulative=False) # Plots a non cumulative graph for 30 common words
